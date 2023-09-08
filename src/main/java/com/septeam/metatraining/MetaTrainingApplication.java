38835243fbda6cee239f710abde17de27c4c9bbb
CodeJugller <jym7546@gmail.com>
2023-09-08 10:49:33 +0900

    Project configuration

---
+++ src/main/java/com/septeam/metatraining/MetaTrainingApplication.java
@@ -0,0 +1,13 @@
+package com.septeam.metatraining;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MetaTrainingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MetaTrainingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/septeam/metatraining/MetaTrainingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.septeam.metatraining;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MetaTrainingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
