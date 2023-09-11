b590956478696bc2e2cae1f0e01e27340941a071
CodeJugller <jym7546@gmail.com>
2023-09-11 18:07:51 +0900

    Revert "[#1] POST CUD 구현"
    This reverts commit 26edec4d72336876ae2014ba7c9c93075c3d9d15.

--- src/main/java/com/septeam/metatraining/post/command/domain/repository/PostRepository.java
+++
@@ -1,9 +0,0 @@
-package com.septeam.metatraining.post.command.domain.repository;
-
-import com.septeam.metatraining.post.command.domain.aggregate.entity.Post;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface PostRepository extends JpaRepository<Post, Long> {
-}
