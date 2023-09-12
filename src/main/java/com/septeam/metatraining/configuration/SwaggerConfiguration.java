669a821891c1c400845ca868745520feff6f33b9
JeonTaeHyeon <jth660084@gmail.com>
2023-09-12 09:28:43 +0900

    [Closes #4]Feat : Init Swagger

---
+++ src/main/java/com/septeam/metatraining/configuration/SwaggerConfiguration.java
@@ -0,0 +1,29 @@
+package com.septeam.metatraining.configuration;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+import org.springdoc.core.GroupedOpenApi;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@OpenAPIDefinition(
+        info = @Info(title = "Meta Training",
+                description = "API Statement for Meta Training",
+                version = "v1"
+        )
+)
+
+public class SwaggerConfiguration {
+
+    @Bean
+    public GroupedOpenApi group1(){
+        String[] paths = {"/**"};
+
+        return GroupedOpenApi
+                .builder()
+                .group("Meta Training Swagger v1")
+                .pathsToMatch(paths)
+                .build();
+    }
+}
